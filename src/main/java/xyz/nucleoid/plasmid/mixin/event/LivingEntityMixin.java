9d7e18b40b29a96c2364796393a8b8f841f0c18a
Draylar <samuelhimes@gmail.com>
2020-08-13 16:08:43 -0500

    add entity drop loot event

--- src/main/java/xyz/nucleoid/plasmid/mixin/event/LivingEntityMixin.java
+++ src/main/java/xyz/nucleoid/plasmid/mixin/event/LivingEntityMixin.java
@@ -1,17 +1,33 @@
 package xyz.nucleoid.plasmid.mixin.event;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContext;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.nucleoid.plasmid.game.GameWorld;
 import xyz.nucleoid.plasmid.game.event.EntityDeathListener;
+import xyz.nucleoid.plasmid.game.event.EntityDropLootListener;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin extends Entity {
+    private LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
     @Inject(method = "onDeath", at = @At("HEAD"), cancellable = true)
     private void callDeathListener(DamageSource source, CallbackInfo ci) {
         LivingEntity entity = (LivingEntity) (Object) this;
@@ -31,5 +47,32 @@ public class LivingEntityMixin {
                 ci.cancel();
             }
         }
+    }
+
+    @Redirect(method = "dropLoot", at = @At(value = "INVOKE", target = "Lnet/minecraft/loot/LootTable;generateLoot(Lnet/minecraft/loot/context/LootContext;Ljava/util/function/Consumer;)V"))
+    private void modifyDroppedLoot(LootTable lootTable, LootContext context, Consumer<ItemStack> lootConsumer) {
+        List<ItemStack> droppedStacks = lootTable.generateLoot(context);
+
+        // default stack dropping for client
+        if (world.isClient) {
+            droppedStacks.forEach(this::dropStack);
+            return;
+        }
+
+        GameWorld gameWorld = GameWorld.forWorld(world);
+
+        if(gameWorld != null && gameWorld.containsEntity((LivingEntity) (Object) this)) {
+            TypedActionResult<List<ItemStack>> result = gameWorld.invoker(EntityDropLootListener.EVENT).onDropLoot((LivingEntity) (Object) this, droppedStacks);
+
+            // drop potentially modified stacks from listeners
+            if(result.getResult() != ActionResult.FAIL) {
+                result.getValue().forEach(this::dropStack);
+            }
+
+            return;
+        }
+
+        // default stack dropping for non-gameworld on server
+        droppedStacks.forEach(this::dropStack);
     }
 }

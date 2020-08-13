7807b5eaf09139ca22aec851bdeb62d92e73e04c
hayden <haydenv06@gmail.com>
2020-08-13 16:34:18 +1000

    nuke Old custom item/block code

--- src/main/java/xyz/nucleoid/plasmid/mixin/custom/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/xyz/nucleoid/plasmid/mixin/custom/ServerPlayNetworkHandlerMixin.java
@@ -2,7 +2,6 @@ package xyz.nucleoid.plasmid.mixin.custom;
 
 import xyz.nucleoid.plasmid.game.GameWorld;
 import xyz.nucleoid.plasmid.game.event.HandSwingListener;
-import xyz.nucleoid.plasmid.item.CustomItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
@@ -22,12 +21,6 @@ public class ServerPlayNetworkHandlerMixin {
     @Inject(method = "onHandSwing", at = @At("HEAD"))
     private void onHandSwing(HandSwingC2SPacket packet, CallbackInfo ci) {
         Hand hand = packet.getHand();
-
-        ItemStack stack = this.player.getStackInHand(hand);
-        CustomItem custom = CustomItem.match(stack);
-        if (custom != null) {
-            custom.onSwingHand(this.player, hand);
-        }
 
         GameWorld gameWorld = GameWorld.forWorld(this.player.world);
         if (gameWorld != null && gameWorld.containsPlayer(this.player)) {

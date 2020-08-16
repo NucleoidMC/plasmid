489852ea27b2ae4f0e997ce62eda3357f49c994c
SuperCoder7979 <k.pranav@gmail.com>
2020-08-16 10:04:49 -0400

    refactor tree generation

--- src/main/java/xyz/nucleoid/plasmid/game/gen/feature/GrassGen.java
+++ src/main/java/xyz/nucleoid/plasmid/game/gen/feature/GrassGen.java
@@ -11,16 +11,19 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ServerWorldAccess;
 
 public class GrassGen implements MapGen {
-	public static final GrassGen INSTANCE = new GrassGen();
-
-	private static final WeightedList<BlockState> STATES = new WeightedList<BlockState>()
+	public static final GrassGen INSTANCE = new GrassGen(new WeightedList<BlockState>()
 			.add(Blocks.GRASS.getDefaultState(), 32)
 			.add(Blocks.DANDELION.getDefaultState(), 1)
-			.add(Blocks.POPPY.getDefaultState(), 1);
+			.add(Blocks.POPPY.getDefaultState(), 1));
+	private final WeightedList<BlockState> states;
+
+	public GrassGen(WeightedList<BlockState> states) {
+		this.states = states;
+	}
 
 	@Override
 	public void generate(ServerWorldAccess world, BlockPos pos, Random random) {
-		BlockState state = STATES.pickRandom(random);
+		BlockState state = states.pickRandom(random);
 
 		for (int i = 0; i < 16; i++) {
 			int aX = random.nextInt(8) - random.nextInt(8);

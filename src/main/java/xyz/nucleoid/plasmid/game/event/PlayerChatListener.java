67ce60edbd25f502f35d27e3879e8b2f4907fa5f
Luke Tonon <41268323+SmellyModder@users.noreply.github.com>
2020-08-22 14:41:14 -0400

    Add PlayerChatListener (#36)
    * Added BroadcastChatMessageListener
    
    * Include text of message that was sent
    
    * Fixed event logic
    
    * Renamed BroadcastChatMessageListener to PlayerChatListener

---
+++ src/main/java/xyz/nucleoid/plasmid/game/event/PlayerChatListener.java
@@ -0,0 +1,32 @@
+package xyz.nucleoid.plasmid.game.event;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+
+/**
+ * Called when any {@link net.minecraft.server.network.ServerPlayerEntity} sends a message in chat in a {@link xyz.nucleoid.plasmid.game.GameWorld}.
+ *
+ * <p>Upon return:
+ * <ul>
+ * <li>{@link ActionResult#SUCCESS} cancels further processing and allows the message to be sent.
+ * <li>{@link ActionResult#FAIL} cancels further processing and the message being sent.
+ * <li>{@link ActionResult#PASS} moves on to the next listener.
+ * </ul>
+ *
+ */
+public interface PlayerChatListener {
+    EventType<PlayerChatListener> EVENT = EventType.create(PlayerChatListener.class, listeners -> (message, sender) -> {
+        for (PlayerChatListener listener : listeners) {
+            ActionResult result = listener.onSendChatMessage(message, sender);
+
+            if (result != ActionResult.PASS) {
+                return result;
+            }
+        }
+
+        return ActionResult.PASS;
+    });
+
+    ActionResult onSendChatMessage(Text message, ServerPlayerEntity sender);
+}

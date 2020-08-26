6c3a0a26593046c168282cfa08af1f7642982021
SmellyModder <smellysocks543@gmail.com>
2020-08-26 01:54:04 -0400

    Added useful way to store server data

---
+++ src/main/java/xyz/nucleoid/plasmid/storage/ServerStorage.java
@@ -0,0 +1,42 @@
+package xyz.nucleoid.plasmid.storage;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.Identifier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implemented in classes that will store NBT data on the server.
+ * This interface is useful for storing data about games, e.g. player scores.
+ *
+ * @see {@link net.minecraft.world.PersistentState}
+ */
+public interface ServerStorage {
+    Map<Identifier, ServerStorage> STORAGES = new HashMap<>();
+
+    /**
+     * Adds a {@link ServerStorage} to the {@link #STORAGES} map and returns the {@link ServerStorage}.
+     * Use this to have your {@link ServerStorage} be saved and loaded.
+     * @param key - The id of the storage.
+     * @param storage - The {@link ServerStorage} to add to the @link #STORAGES} map.
+     * @param <S> - The type of {@link ServerStorage}
+     * @return - The supplied {@link ServerStorage}.
+     */
+    static <S extends ServerStorage> S createStorage(Identifier key, S storage) {
+        STORAGES.put(key, storage);
+        return storage;
+    }
+
+    /**
+     * Called when saving this {@link ServerStorage} to NBT.
+     * @return - The serialized NBT data of this {@link ServerStorage}
+     */
+    CompoundTag toTag();
+
+    /**
+     * Called when loading the saved NBT data for this {@link ServerStorage}.
+     * @param tag - The deserialized NBT data of this {@link ServerStorage}.
+     */
+    void fromTag(CompoundTag tag);
+}
---
+++ src/main/java/xyz/nucleoid/plasmid/storage/ServerStorageManager.java
@@ -0,0 +1,48 @@
+package xyz.nucleoid.plasmid.storage;
+
+import net.fabricmc.fabric.api.util.NbtType;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.PersistentState;
+import xyz.nucleoid.plasmid.Plasmid;
+
+public final class ServerStorageManager extends PersistentState {
+    private static final String KEY = Plasmid.ID + "_storage";
+
+    ServerStorageManager() {
+        super(KEY);
+    }
+
+    public static ServerStorageManager get(ServerWorld world) {
+        return world.getPersistentStateManager().getOrCreate(() -> new ServerStorageManager(), KEY);
+    }
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+        ListTag storageList = new ListTag();
+        ServerStorage.STORAGES.entrySet().forEach((entry) -> {
+            CompoundTag storageTag = entry.getValue().toTag();
+            storageTag.putString("id", entry.getKey().toString());
+            storageList.add(storageTag);
+        });
+        tag.put("storages", storageList);
+        return tag;
+    }
+
+    @Override
+    public void fromTag(CompoundTag tag) {
+        ListTag storageTags = tag.getList("storages", NbtType.COMPOUND);
+
+        for (int i = 0; i < storageTags.size(); i++) {
+            CompoundTag storageTag = storageTags.getCompound(i);
+            ServerStorage.STORAGES.get(new Identifier(storageTag.getString("id"))).fromTag(storageTag);
+        }
+    }
+
+    @Override
+    public boolean isDirty() {
+        return true;
+    }
+}

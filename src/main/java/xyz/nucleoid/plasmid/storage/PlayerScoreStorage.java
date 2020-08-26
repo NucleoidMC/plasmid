39fd2cc7560842fa07143d7512cc68d7db32cd8e
SmellyModder <smellysocks543@gmail.com>
2020-08-26 02:58:47 -0400

    Suggested Changes

--- src/main/java/xyz/nucleoid/plasmid/storage/PlayerScoreStorage.java
+++ src/main/java/xyz/nucleoid/plasmid/storage/PlayerScoreStorage.java
@@ -10,13 +10,27 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * A simple class for storing the scores of players.
+ * 'Score' can mean any number.
+ */
 public class PlayerScoreStorage implements ServerStorage {
     public final Map<UUID, Float> scoreMap = new HashMap<>();
 
+    /**
+     * Puts a player's UUID and score onto the {@link #scoreMap}.
+     * @param player - The player to assign the score for.
+     * @param score - The score for the player.
+     */
     public void putPlayerScore(ServerPlayerEntity player, float score) {
         this.scoreMap.put(player.getUuid(), score);
     }
 
+    /**
+     * Gets the player's score.
+     * @param player - The player to get the score for.
+     * @return - The player's score or 0 if the player isn't in the {@link #scoreMap}.
+     */
     public float getPlayerScore(ServerPlayerEntity player) {
         return this.scoreMap.getOrDefault(player, 0.0F);
     }

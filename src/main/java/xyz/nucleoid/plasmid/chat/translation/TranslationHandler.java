4c71f9e3bd5f1c3951f67cbf1b8ecbd06fa645c9
CatCore <arthur.bambou@gmail.com>
2020-08-27 23:20:34 +0200

    fix text siblings loosing their style

--- src/main/java/xyz/nucleoid/plasmid/chat/translation/TranslationHandler.java
+++ src/main/java/xyz/nucleoid/plasmid/chat/translation/TranslationHandler.java
@@ -3,9 +3,7 @@ package xyz.nucleoid.plasmid.chat.translation;
 import fr.catcore.server.translations.ServerTranslations;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.*;
 import net.minecraft.util.Language;
 
 import java.util.ArrayList;
@@ -34,7 +32,30 @@ public class TranslationHandler {
             return ServerTranslations.getMessageForPlayer(translatableText, serverPlayerEntity);
         }
         if (SERVER_SIDE_KEYS.contains(translatableText.getKey())) {
-            return new LiteralText(translatableText.getString()).setStyle(translatableText.getStyle());
+            Style style = translatableText.getStyle();
+            String string = translatableText.getString();
+            List<Text> siblings = new ArrayList<>();
+            for (Text text : translatableText.getSiblings()) {
+                string = string.replace(text.getString(), "");
+                if (text instanceof TranslatableText) siblings.add(new LiteralText(text.getString()).setStyle(text.getStyle()));
+                else if (text instanceof BaseText) {
+                    for (Text text1 : translatableText.getSiblings()) {
+                        string = string.replace(text1.getString(), "");
+                        if (text1 instanceof TranslatableText) siblings.add(new LiteralText(text1.getString()).setStyle(text1.getStyle()));
+                        else {
+                            siblings.add(text1);
+                        }
+
+                    }
+                } else {
+                    siblings.add(text);
+                }
+            }
+            MutableText literalText = new LiteralText(string).setStyle(style);
+            for (Text sibling : siblings) {
+                literalText = literalText.append(sibling);
+            }
+            return literalText;
         }
         return translatableText;
     }

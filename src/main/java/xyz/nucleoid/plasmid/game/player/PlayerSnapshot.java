689d7a085a1d7f0ada5fddf38065174352160dc3
gegy1000 <gegy1000@gmail.com>
2020-11-18 22:20:06 +0200

    handle player snapshots by writing nbt

--- src/main/java/xyz/nucleoid/plasmid/game/player/PlayerSnapshot.java
+++ src/main/java/xyz/nucleoid/plasmid/game/player/PlayerSnapshot.java
@@ -1,59 +1,26 @@
 package xyz.nucleoid.plasmid.game.player;
 
-import net.minecraft.entity.attribute.AttributeContainer;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 public final class PlayerSnapshot {
     private final RegistryKey<World> dimension;
     private final Vec3d position;
     private final float yaw;
     private final float pitch;
-    private final GameMode gameMode;
-    private final DefaultedList<ItemStack> inventory;
-    private final DefaultedList<ItemStack> enderInventory;
-    private final Collection<StatusEffectInstance> potionEffects;
-    private final int totalExperience;
-    private final int experienceLevel;
-    private final float experienceProgress;
+    private final CompoundTag playerData;
 
-    private PlayerSnapshot(
-            RegistryKey<World> dimension, Vec3d position, float yaw, float pitch,
-            GameMode gameMode,
-            DefaultedList<ItemStack> inventory,
-            DefaultedList<ItemStack> enderInventory,
-            Collection<StatusEffectInstance> potionEffects,
-            int totalExperience, int experienceLevel, float experienceProgress
-    ) {
+    private PlayerSnapshot(RegistryKey<World> dimension, Vec3d position, float yaw, float pitch, CompoundTag playerData) {
         this.dimension = dimension;
         this.position = position;
         this.yaw = yaw;
         this.pitch = pitch;
-        this.gameMode = gameMode;
-        this.inventory = inventory;
-        this.enderInventory = enderInventory;
-        this.potionEffects = potionEffects;
-        this.totalExperience = totalExperience;
-        this.experienceLevel = experienceLevel;
-        this.experienceProgress = experienceProgress;
+        this.playerData = playerData;
     }
 
     public static PlayerSnapshot take(ServerPlayerEntity player) {
@@ -61,79 +28,22 @@ public final class PlayerSnapshot {
         Vec3d position = player.getPos();
         float yaw = player.yaw;
         float pitch = player.pitch;
-        GameMode gameMode = player.interactionManager.getGameMode();
 
-        DefaultedList<ItemStack> inventory = snapshotInventory(player.inventory);
-        DefaultedList<ItemStack> enderInventory = snapshotInventory(player.getEnderChestInventory());
+        CompoundTag playerData = player.toTag(new CompoundTag());
 
-        List<StatusEffectInstance> potionEffects = player.getStatusEffects().stream()
-                .map(StatusEffectInstance::new)
-                .collect(Collectors.toList());
-
-        int totalExperience = player.totalExperience;
-        int experienceLevel = player.experienceLevel;
-        float experienceProgress = player.experienceProgress;
-
-        return new PlayerSnapshot(
-                dimension, position, yaw, pitch,
-                gameMode,
-                inventory, enderInventory,
-                potionEffects,
-                totalExperience, experienceLevel, experienceProgress
-        );
+        return new PlayerSnapshot(dimension, position, yaw, pitch, playerData);
     }
 
     public void restore(ServerPlayerEntity player) {
         ServerWorld world = player.getServerWorld().getServer().getWorld(this.dimension);
 
         player.setGameMode(GameMode.ADVENTURE);
-
-        this.restoreInventory(player.inventory, this.inventory);
-        this.restoreInventory(player.getEnderChestInventory(), this.enderInventory);
-
-        player.clearStatusEffects();
-        for (StatusEffectInstance potionEffect : this.potionEffects) {
-            player.addStatusEffect(potionEffect);
-        }
-
-        player.totalExperience = this.totalExperience;
-        player.experienceProgress = this.experienceProgress;
-        player.experienceLevel = this.experienceLevel;
-
         player.teleport(world, this.position.x, this.position.y, this.position.z, this.yaw, this.pitch);
-        player.setGameMode(this.gameMode);
+
+        player.fromTag(this.playerData);
 
         player.setFireTicks(0);
         player.stopFallFlying();
         player.fallDistance = 0.0F;
-
-        AttributeContainer attributes = player.getAttributes();
-        for (EntityAttribute attribute : Registry.ATTRIBUTE) {
-            if (!attributes.hasAttribute(attribute)) {
-                continue;
-            }
-
-            EntityAttributeInstance attributeInstance = attributes.getCustomInstance(attribute);
-            Set<UUID> modifiers = attributeInstance.getModifiers().stream()
-                    .map(EntityAttributeModifier::getId)
-                    .collect(Collectors.toSet());
-
-            modifiers.forEach(attributeInstance::removeModifier);
-        }
-    }
-
-    private void restoreInventory(Inventory inventory, DefaultedList<ItemStack> from) {
-        inventory.clear();
-        for (int i = 0; i < from.size(); i++) {
-            inventory.setStack(i, from.get(i));
-        }
-    }
-
-    private static DefaultedList<ItemStack> snapshotInventory(Inventory inventory) {
-        DefaultedList<ItemStack> copy = DefaultedList.ofSize(inventory.size(), ItemStack.EMPTY);
-        for (int i = 0; i < copy.size(); i++) {
-            copy.set(i, inventory.getStack(i));
-        }
-        return copy;
     }
 }

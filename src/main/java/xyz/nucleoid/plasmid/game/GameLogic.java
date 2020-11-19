929558edfd26ccda4b30b482ec1fefb7d5e3fdcb
gegy1000 <gegy1000@gmail.com>
2020-11-19 16:35:29 +0200

    Add documentation for GameLogic and GameSpace

--- src/main/java/xyz/nucleoid/plasmid/game/GameLogic.java
+++ src/main/java/xyz/nucleoid/plasmid/game/GameLogic.java
@@ -23,21 +23,49 @@ public final class GameLogic {
         this.space = space;
     }
 
+    /**
+     * Sets a global rule on this {@link GameLogic}.
+     *
+     * @param rule the rule type
+     * @param result the behavior to be used for this rule
+     * @return the updated {@link GameLogic}
+     */
     public GameLogic setRule(GameRule rule, RuleResult result) {
         this.rules.put(rule, result);
         return this;
     }
 
+    /**
+     * Attaches a listener for the given event to this {@link GameLogic}
+     *
+     * @param event the event type to listen for
+     * @param listener the listener to call when this event is invoked
+     * @param <T> the event listener type
+     * @return the updated {@link GameLogic}
+     */
     public <T> GameLogic on(EventType<T> event, T listener) {
         this.listeners.add(event, listener);
         return this;
     }
 
+    /**
+     * Adds a resource to this {@link GameLogic} object that will be automatically closed when the {@link GameLogic}
+     * instance is changed on the parent {@link GameSpace}.
+     *
+     * This differs from {@link GameSpace#addResource(AutoCloseable)}, which will be closed when the {@link GameSpace}
+     * itself is closed.
+     *
+     * @param resource the resource to close when this {@link GameLogic} closes
+     * @return the updated {@link GameLogic}
+     */
     public GameLogic addResource(AutoCloseable resource) {
         this.resources.add(resource);
         return this;
     }
 
+    /**
+     * @return the parent {@link GameSpace} that this logic acts upon
+     */
     public GameSpace getSpace() {
         return this.space;
     }

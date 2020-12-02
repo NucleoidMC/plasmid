7b1d757f0c1f013f4a9a0081f8cf3f0e95cfc179
gegy1000 <gegy1000@gmail.com>
2020-12-02 10:08:29 +0200

    use codec for config serialization

--- src/main/java/xyz/nucleoid/plasmid/config/PlasmidConfig.java
+++ src/main/java/xyz/nucleoid/plasmid/config/PlasmidConfig.java
@@ -2,7 +2,13 @@ package xyz.nucleoid.plasmid.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.annotations.SerializedName;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import joptsimple.internal.Strings;
 import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
@@ -17,16 +23,31 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 public final class PlasmidConfig {
     private static final Path PATH = Paths.get("config/plasmid.json");
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final JsonParser JSON_PARSER = new JsonParser();
+
+    private static final Codec<PlasmidConfig> CODEC = RecordCodecBuilder.create(instance -> {
+        return instance.group(
+            Codec.STRING.optionalFieldOf("error_reporting_webhook_url", "").forGetter(config -> config.errorReportingWebhookUrl)
+        ).apply(instance, PlasmidConfig::new);
+    });
 
     private static PlasmidConfig instance;
 
-    @SerializedName("error_reporting_webhook_url")
-    private String errorReportingWebhookUrl = "";
+    private final String errorReportingWebhookUrl;
+
+    private PlasmidConfig(String errorReportingWebhookUrl) {
+        this.errorReportingWebhookUrl = errorReportingWebhookUrl;
+    }
+
+    private PlasmidConfig() {
+        this.errorReportingWebhookUrl = "";
+    }
 
     @Nullable
     public String getErrorReportingWebhookUrl() {
@@ -54,7 +75,9 @@ public final class PlasmidConfig {
 
     private static PlasmidConfig loadConfig() {
         try (InputStream input = Files.newInputStream(PATH)) {
-            return GSON.fromJson(new InputStreamReader(input), PlasmidConfig.class);
+            JsonElement json = JSON_PARSER.parse(new InputStreamReader(input));
+            DataResult<PlasmidConfig> result = CODEC.decode(JsonOps.INSTANCE, json).map(Pair::getFirst);
+            return result.result().orElseGet(PlasmidConfig::new);
         } catch (IOException e) {
             Plasmid.LOGGER.warn("Failed to load plasmid config", e);
             return new PlasmidConfig();
@@ -64,7 +87,11 @@ public final class PlasmidConfig {
     private static PlasmidConfig createDefaultConfig() {
         PlasmidConfig config = new PlasmidConfig();
         try (OutputStream output = Files.newOutputStream(PATH)) {
-            IOUtils.write(GSON.toJson(config), output, StandardCharsets.UTF_8);
+            Optional<JsonElement> result = CODEC.encodeStart(JsonOps.INSTANCE, config).result();
+            if (result.isPresent()) {
+                JsonElement json = result.get();
+                IOUtils.write(GSON.toJson(json), output, StandardCharsets.UTF_8);
+            }
         } catch (IOException e) {
             Plasmid.LOGGER.warn("Failed to create default plasmid config", e);
         }

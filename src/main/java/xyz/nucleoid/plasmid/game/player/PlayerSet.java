806b0c57fc48bc5ad777cc373269b28a3db143a0
gegy1000 <gegy1000@gmail.com>
2020-12-10 12:53:21 +0200

    add PlayerSet.EMPTY

--- src/main/java/xyz/nucleoid/plasmid/game/player/PlayerSet.java
+++ src/main/java/xyz/nucleoid/plasmid/game/player/PlayerSet.java
@@ -7,14 +7,46 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xyz.nucleoid.plasmid.util.PlayerRef;
 
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.UUID;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public interface PlayerSet extends PlayerOps, Iterable<ServerPlayerEntity> {
+    PlayerSet EMPTY = new PlayerSet() {
+        @Override
+        public boolean contains(UUID id) {
+            return false;
+        }
+
+        @Override
+        @Nullable
+        public ServerPlayerEntity getEntity(UUID id) {
+            return null;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public PlayerSet copy() {
+            return EMPTY;
+        }
+
+        @NotNull
+        @Override
+        public Iterator<ServerPlayerEntity> iterator() {
+            return Collections.emptyIterator();
+        }
+    };
+
     boolean contains(UUID id);
 
     @Nullable

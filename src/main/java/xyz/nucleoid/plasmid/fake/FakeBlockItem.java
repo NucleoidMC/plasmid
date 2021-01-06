14f0cf12922acc1825a12d6de3937b12377345a2
hYdos <haydenv06@gmail.com>
2021-01-07 08:28:49 +1000

    hopefully fix FakeBlockItem

--- src/main/java/xyz/nucleoid/plasmid/fake/FakeBlockItem.java
+++ src/main/java/xyz/nucleoid/plasmid/fake/FakeBlockItem.java
@@ -3,19 +3,28 @@ package xyz.nucleoid.plasmid.fake;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
 
-@Deprecated
-public class FakeBlockItem extends BlockItem implements FakeItem {
+public class FakeBlockItem extends Item implements FakeItem {
 
-    private final Block clientSideBlock;
+    private final Block block;
+    private final Item proxyItem;
 
-    public FakeBlockItem(Block serverSideBlock, Block clientSideBlock, Settings settings) {
-        super(serverSideBlock, settings);
-        this.clientSideBlock = clientSideBlock;
+    public FakeBlockItem(Block block, Item proxyItem, Settings settings) {
+        super(settings);
+        this.block = block;
+        this.proxyItem = proxyItem;
     }
 
     @Override
     public Item asProxy() {
-        return this.clientSideBlock.asItem();
+        return this.proxyItem;
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        context.getWorld().setBlockState(context.getBlockPos(), this.block.getDefaultState());
+        return ActionResult.SUCCESS;
     }
 }

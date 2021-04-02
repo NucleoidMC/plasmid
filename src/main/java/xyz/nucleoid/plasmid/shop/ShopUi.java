21e11ad6eba39d5d015f69519d1fc00ba1f0f10a
Gegy <gegy1000@gmail.com>
2021-04-02 21:27:12 +0200

    never restrict player access to shop ui

--- src/main/java/xyz/nucleoid/plasmid/shop/ShopUi.java
+++ src/main/java/xyz/nucleoid/plasmid/shop/ShopUi.java
@@ -55,6 +55,11 @@ public final class ShopUi implements NamedScreenHandlerFactory {
             private void resendInventory() {
                 serverPlayer.onHandlerRegistered(this, this.getStacks());
             }
+
+            @Override
+            public boolean isNotRestricted(PlayerEntity player) {
+                return true;
+            }
         };
     }
 }

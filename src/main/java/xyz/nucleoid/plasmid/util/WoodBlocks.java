5d16e83035b872a62ecea8532e7cd05bc9c739b9
Hugman <contact.hugman@gmail.com>
2021-04-29 17:21:05 +0200

    WoodBlocks: null returns and block-to-block conversions

--- src/main/java/xyz/nucleoid/plasmid/util/WoodBlocks.java
+++ src/main/java/xyz/nucleoid/plasmid/util/WoodBlocks.java
@@ -1,31 +1,26 @@
 package xyz.nucleoid.plasmid.util;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 
-public class WoodBlocks {
-    public static BlockState saplingOf(BlockState state) {
-        Block block = state.getBlock();
-        if (block == Blocks.OAK_LEAVES) return Blocks.OAK_SAPLING.getDefaultState();
-        if (block == Blocks.BIRCH_LEAVES) return Blocks.BIRCH_SAPLING.getDefaultState();
-        if (block == Blocks.SPRUCE_LEAVES) return Blocks.SPRUCE_SAPLING.getDefaultState();
-        if (block == Blocks.JUNGLE_LEAVES) return Blocks.JUNGLE_SAPLING.getDefaultState();
-        if (block == Blocks.ACACIA_LEAVES) return Blocks.ACACIA_SAPLING.getDefaultState();
-        if (block == Blocks.DARK_OAK_LEAVES) return Blocks.DARK_OAK_SAPLING.getDefaultState();
-
-        return Blocks.AIR.getDefaultState();
+public final class WoodBlocks {
+    public static Block saplingOf(Block block) {
+        if(block == Blocks.OAK_LEAVES) { return Blocks.OAK_SAPLING; }
+        else if(block == Blocks.BIRCH_LEAVES) { return Blocks.BIRCH_SAPLING; }
+        else if(block == Blocks.SPRUCE_LEAVES) { return Blocks.SPRUCE_SAPLING; }
+        else if(block == Blocks.JUNGLE_LEAVES) { return Blocks.JUNGLE_SAPLING; }
+        else if(block == Blocks.ACACIA_LEAVES) { return Blocks.ACACIA_SAPLING; }
+        else if(block == Blocks.DARK_OAK_LEAVES) { return Blocks.DARK_OAK_SAPLING; }
+        else { return null; }
     }
 
-    public static BlockState planksOf(BlockState state) {
-        Block block = state.getBlock();
-        if (block == Blocks.OAK_LOG) return Blocks.OAK_PLANKS.getDefaultState();
-        if (block == Blocks.BIRCH_LOG) return Blocks.BIRCH_PLANKS.getDefaultState();
-        if (block == Blocks.SPRUCE_LOG) return Blocks.SPRUCE_PLANKS.getDefaultState();
-        if (block == Blocks.JUNGLE_LOG) return Blocks.JUNGLE_PLANKS.getDefaultState();
-        if (block == Blocks.ACACIA_LOG) return Blocks.ACACIA_PLANKS.getDefaultState();
-        if (block == Blocks.DARK_OAK_LOG) return Blocks.DARK_OAK_PLANKS.getDefaultState();
-
-        return Blocks.AIR.getDefaultState();
+    public static Block planksOf(Block block) {
+        if(block == Blocks.OAK_LOG) { return Blocks.OAK_PLANKS; }
+        else if(block == Blocks.BIRCH_LOG) { return Blocks.BIRCH_PLANKS; }
+        else if(block == Blocks.SPRUCE_LOG) { return Blocks.SPRUCE_PLANKS; }
+        else if(block == Blocks.JUNGLE_LOG) { return Blocks.JUNGLE_PLANKS; }
+        else if(block == Blocks.ACACIA_LOG) { return Blocks.ACACIA_PLANKS; }
+        else if(block == Blocks.DARK_OAK_LOG) { return Blocks.DARK_OAK_PLANKS; }
+        else { return null; }
     }
 }

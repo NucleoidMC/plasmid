e968199726ee8a4ab97c981d0eb06cb53e7d588a
Hugman <contact.hugman@gmail.com>
2021-05-06 21:57:13 +0200

    Faster methods

--- src/main/java/xyz/nucleoid/plasmid/util/BucketScanner.java
+++ src/main/java/xyz/nucleoid/plasmid/util/BucketScanner.java
@@ -14,218 +14,118 @@ import java.util.*;
 import java.util.function.Predicate;
 
 /**
- * Methods for finding connected blocks as a {@link LongSet}. Use {@link BlockPos#fromLong} to retrieve these positions.
- *
- * @see <a href="https://en.wikipedia.org/wiki/Pixel_connectivity#3-dimensional">3-dimensional pixel connectivity</a>
+ * Methods for finding connected blocks as a {@link LongSet}. Use {@link BlockPos#fromLong} to retrieve returned positions.
  */
 public final class BucketScanner {
     private BucketScanner() {
     }
 
     /**
-     * Finds any 6-connected blocks and puts them in a {@link LongSet}.
+     * Finds any connected blocks and puts them in a {@link LongSet}.
      *
-     * @param origin   the position of the first block
-     * @param depth    the amount of maximum blocks to find
-     * @param ruleTest the rule test for the blocks that can be accepted in the set
+     * @param origin       the position of the first block
+     * @param amount       the amount of maximum blocks to find
+     * @param connectivity the type of scan that should be used to find blocks around a branch end
+     * @param ruleTest     the rule test for the blocks that can be accepted in the set
      */
-    public static LongSet findSix(BlockPos origin, int depth, RuleTest ruleTest, ServerWorld world, Random random) {
-        return findSix(origin, depth, pos -> ruleTest.test(world.getBlockState(pos), random));
+    public static LongSet find(BlockPos origin, int amount, Connectivity connectivity, RuleTest ruleTest, ServerWorld world, Random random) {
+        return find(origin, amount, connectivity, pos -> ruleTest.test(world.getBlockState(pos), random));
     }
 
     /**
-     * Finds any 6-connected blocks and puts them in a {@link LongSet}.
+     * Finds any connected blocks and puts them in a {@link LongSet}.
      *
-     * @param origin the position of the first block
-     * @param depth  the amount of maximum blocks to find
-     * @param tag    the tag for the blocks that can be accepted in the set
+     * @param origin       the position of the first block
+     * @param amount       the amount of maximum blocks to find
+     * @param connectivity the type of scan that should be used to find blocks around a branch end
+     * @param tag          the tag for the blocks that can be accepted in the set
      */
-    public static LongSet findSix(BlockPos origin, int depth, Tag<Block> tag, ServerWorld world) {
-        return findSix(origin, depth, pos -> world.getBlockState(pos).isIn(tag));
+    public static LongSet find(BlockPos origin, int amount, Connectivity connectivity, Tag<Block> tag, ServerWorld world) {
+        return find(origin, amount, connectivity, pos -> world.getBlockState(pos).isIn(tag));
     }
 
     /**
-     * Finds any 6-connected blocks and puts them in a {@link LongSet}.
+     * Finds any connected blocks and puts them in a {@link LongSet}.
      *
-     * @param origin the position of the first block
-     * @param depth  the amount of maximum blocks to find
-     * @param block  the block type that can be accepted in the set
+     * @param origin       the position of the first block
+     * @param amount       the amount of maximum blocks to find
+     * @param connectivity the type of scan that should be used to find blocks around a branch end
+     * @param block        the block type that can be accepted in the set
      */
-    public static LongSet findSix(BlockPos origin, int depth, Block block, ServerWorld world) {
-        return findSix(origin, depth, pos -> world.getBlockState(pos).isOf(block));
+    public static LongSet find(BlockPos origin, int amount, Connectivity connectivity, Block block, ServerWorld world) {
+        return find(origin, amount, connectivity, pos -> world.getBlockState(pos).isOf(block));
     }
 
     /**
-     * Finds any 6-connected blocks and puts them in a {@link LongSet}.
+     * Finds any connected blocks and puts them in a {@link LongSet}.
      *
-     * @param origin    the position of the first block
-     * @param depth     the amount of maximum blocks to find
-     * @param predicate the predicate for the blocks that can be accepted in the set
+     * @param origin       the position of the first block
+     * @param amount       the amount of maximum blocks to find
+     * @param connectivity the type of scan that should be used to find blocks around a branch end
+     * @param predicate    the predicate for the blocks that can be accepted in the set
      */
-    public static LongSet findSix(BlockPos origin, int depth, Predicate<BlockPos> predicate) {
+    public static LongSet find(BlockPos origin, int amount, Connectivity connectivity, Predicate<BlockPos> predicate) {
         LongSet set = new LongArraySet();
         Deque<BlockPos> ends = new ArrayDeque<>();
         ends.push(origin);
-        BlockPos.Mutable local = origin.mutableCopy();
-        while(depth > 0) {
+        BlockPos.Mutable mutable = origin.mutableCopy();
+        while(amount > 0) {
             if(ends.isEmpty()) {
                 return set;
             }
             BlockPos pos = ends.pollLast();
-            for(Direction direction : Direction.values()) {
-                local.set(pos.offset(direction));
-                if(predicate.test(local) && !set.contains(local.asLong()) && !ends.contains(local)) {
-                    ends.push(local.toImmutable());
-                }
-            }
-            set.add(pos.asLong());
-            depth--;
-        }
-        return set;
-    }
-
-    /**
-     * Finds any 18-connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin   the position of the first block
-     * @param depth    the amount of maximum blocks to find
-     * @param ruleTest the rule test for the blocks that can be accepted in the set
-     */
-    public static LongSet findEighteen(BlockPos origin, int depth, RuleTest ruleTest, ServerWorld world, Random random) {
-        return findEighteen(origin, depth, pos -> ruleTest.test(world.getBlockState(pos), random));
-    }
-
-    /**
-     * Finds any 18-connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin the position of the first block
-     * @param depth  the amount of maximum blocks to find
-     * @param tag    the tag for the blocks that can be accepted in the set
-     */
-    public static LongSet findEighteen(BlockPos origin, int depth, Tag<Block> tag, ServerWorld world) {
-        return findEighteen(origin, depth, pos -> world.getBlockState(pos).isIn(tag));
-    }
-
-    /**
-     * Finds any 18-connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin the position of the first block
-     * @param depth  the amount of maximum blocks to find
-     * @param block  the block type that can be accepted in the set
-     */
-    public static LongSet findEighteen(BlockPos origin, int depth, Block block, ServerWorld world) {
-        return findEighteen(origin, depth, pos -> world.getBlockState(pos).isOf(block));
-    }
-
-    /**
-     * Finds any 18-connected blocks and puts them in a {@link LongSet}..
-     *
-     * @param origin    the position of the first block
-     * @param depth     the amount of maximum blocks to find
-     * @param predicate the predicate for the blocks that can be accepted in the set
-     */
-    public static LongSet findEighteen(BlockPos origin, int depth, Predicate<BlockPos> predicate) {
-        LongSet set = new LongArraySet();
-        Deque<BlockPos> ends = new ArrayDeque<>();
-        ends.push(origin);
-        BlockPos.Mutable local = origin.mutableCopy();
-        while(depth > 0) {
-            if(ends.isEmpty()) {
-                return set;
-            }
-            BlockPos pos = ends.pollLast();
-            for(Direction direction : Direction.values()) {
-                local.set(pos.offset(direction));
-                if(predicate.test(local) && !set.contains(local.asLong()) && !ends.contains(local)) {
-                    ends.push(local.toImmutable());
-                }
-            }
-            for(int i = -1; i <= 1; i += 2) {
-                for(int j = -1; j <= 1; j += 2) {
-                    local.set(pos.add(i, j, 0));
-                    if(predicate.test(local) && !set.contains(local.asLong()) && !ends.contains(local)) {
-                        ends.push(local.toImmutable());
-                    }
-                    local.set(pos.add(0, i, j));
-                    if(predicate.test(local) && !set.contains(local.asLong()) && !ends.contains(local)) {
-                        ends.push(local.toImmutable());
-                    }
-                    local.set(pos.add(j, 0, i));
-                    if(predicate.test(local) && !set.contains(local.asLong()) && !ends.contains(local)) {
-                        ends.push(local.toImmutable());
-                    }
-                }
-            }
-            set.add(pos.asLong());
-            depth--;
-        }
-        return set;
-    }
-
-    /**
-     * Finds any 26-connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin   the position of the first block
-     * @param depth    the amount of maximum blocks to find
-     * @param ruleTest the rule test for the blocks that can be accepted in the set
-     */
-    public static LongSet findTwentySix(BlockPos origin, int depth, RuleTest ruleTest, ServerWorld world, Random random) {
-        return findTwentySix(origin, depth, pos -> ruleTest.test(world.getBlockState(pos), random));
-    }
-
-    /**
-     * Finds any 26-connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin the position of the first block
-     * @param depth  the amount of maximum blocks to find
-     * @param tag    the tag for the blocks that can be accepted in the set
-     */
-    public static LongSet findTwentySix(BlockPos origin, int depth, Tag<Block> tag, ServerWorld world) {
-        return findTwentySix(origin, depth, pos -> world.getBlockState(pos).isIn(tag));
-    }
-
-    /**
-     * Finds any 26-connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin the position of the first block
-     * @param depth  the amount of maximum blocks to find
-     * @param block  the block type that can be accepted in the set
-     */
-    public static LongSet findTwentySix(BlockPos origin, int depth, Block block, ServerWorld world) {
-        return findTwentySix(origin, depth, pos -> world.getBlockState(pos).isOf(block));
-    }
-
-    /**
-     * Finds any 26-connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin    the position of the first block
-     * @param depth     the amount of maximum blocks to find
-     * @param predicate the predicate for the blocks that can be accepted in the set
-     */
-    public static LongSet findTwentySix(BlockPos origin, int depth, Predicate<BlockPos> predicate) {
-        LongSet set = new LongArraySet();
-        Deque<BlockPos> ends = new ArrayDeque<>();
-        ends.push(origin);
-        BlockPos.Mutable local = origin.mutableCopy();
-        while(depth > 0) {
-            if(ends.isEmpty()) {
-                return set;
-            }
-            BlockPos pos = ends.pollLast();
-            for(int x = -1; x <= 1; x++) {
-                for(int y = -1; y <= 1; y++) {
-                    for(int z = -1; z <= 1; z++) {
-                        if(x == 0 && y == 0 && z == 0) continue;
-                        local.set(pos.add(x, y, z));
-                        if(predicate.test(local) && !set.contains(local.asLong()) && !ends.contains(local)) {
-                            ends.push(local.toImmutable());
+            switch(connectivity) {
+                case EIGHTEEN:
+                    for(int i = -1; i <= 1; i += 2) {
+                        for(int j = -1; j <= 1; j += 2) {
+                            mutable.set(pos.add(i, j, 0));
+                            if(predicate.test(mutable) && !set.contains(mutable.asLong()) && !ends.contains(mutable)) {
+                                ends.push(mutable.toImmutable());
+                            }
+                            mutable.set(pos.add(0, i, j));
+                            if(predicate.test(mutable) && !set.contains(mutable.asLong()) && !ends.contains(mutable)) {
+                                ends.push(mutable.toImmutable());
+                            }
+                            mutable.set(pos.add(j, 0, i));
+                            if(predicate.test(mutable) && !set.contains(mutable.asLong()) && !ends.contains(mutable)) {
+                                ends.push(mutable.toImmutable());
+                            }
                         }
                     }
-                }
+                case SIX:
+                    for(Direction direction : Direction.values()) {
+                        mutable.set(pos.offset(direction));
+                        if(predicate.test(mutable) && !set.contains(mutable.asLong()) && !ends.contains(mutable)) {
+                            ends.push(mutable.toImmutable());
+                        }
+                    }
+                    break;
+                case TWENTY_SIX:
+                    for(int x = -1; x <= 1; x++) {
+                        for(int y = -1; y <= 1; y++) {
+                            for(int z = -1; z <= 1; z++) {
+                                if(x == 0 && y == 0 && z == 0) continue;
+                                mutable.set(pos.add(x, y, z));
+                                if(predicate.test(mutable) && !set.contains(mutable.asLong()) && !ends.contains(mutable)) {
+                                    ends.push(mutable.toImmutable());
+                                }
+                            }
+                        }
+                    }
+                    break;
             }
             set.add(pos.asLong());
-            depth--;
+            amount--;
         }
         return set;
+    }
+
+    /**
+     * @see <a href="https://en.wikipedia.org/wiki/Pixel_connectivity#3-dimensional">3-dimensional pixel connectivity</a>
+     */
+    public enum Connectivity {
+        SIX,
+        EIGHTEEN,
+        TWENTY_SIX
     }
 }

a223d147f79627935e973952902b5d313083ea50
Hugman <contact.hugman@gmail.com>
2021-05-08 11:48:35 +0200

    BucketScanner: small reformat

--- src/main/java/xyz/nucleoid/plasmid/util/BucketScanner.java
+++ src/main/java/xyz/nucleoid/plasmid/util/BucketScanner.java
@@ -24,54 +24,21 @@ public final class BucketScanner {
     /**
      * Finds any connected blocks and puts them in a {@link LongSet}.
      *
-     * @param origin   the position of the first block
-     * @param amount   the amount of maximum blocks to find
-     * @param ruleTest the rule test for the blocks that can be accepted in the set
-     */
-    public static LongSet find(BlockPos origin, Type depth, Connectivity connectivity, int amount, RuleTest ruleTest, ServerWorld world, Random random) {
-        return find(origin, depth, connectivity, amount, pos -> ruleTest.test(world.getBlockState(pos), random));
-    }
-
-    /**
-     * Finds any connected blocks and puts them in a {@link LongSet}.
-     *
      * @param origin the position of the first block
      * @param amount the amount of maximum blocks to find
-     * @param tag    the tag for the blocks that can be accepted in the set
-     */
-    public static LongSet find(BlockPos origin, Type depth, Connectivity connectivity, int amount, Tag<Block> tag, ServerWorld world) {
-        return find(origin, depth, connectivity, amount, pos -> world.getBlockState(pos).isIn(tag));
-    }
-
-    /**
-     * Finds any connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin the position of the first block
-     * @param amount the amount of maximum blocks to find
-     * @param block  the block type that can be accepted in the set
-     */
-    public static LongSet find(BlockPos origin, Type depth, Connectivity connectivity, int amount, Block block, ServerWorld world) {
-        return find(origin, depth, connectivity, amount, pos -> world.getBlockState(pos).isOf(block));
-    }
-
-    /**
-     * Finds any connected blocks and puts them in a {@link LongSet}.
-     *
-     * @param origin    the position of the first block
-     * @param amount    the amount of maximum blocks to find
      * @param predicate the predicate for the blocks that can be accepted in the set
      */
-    public static LongSet find(BlockPos origin, Type depth, Connectivity connectivity, int amount, Predicate<BlockPos> predicate) {
+    public static LongSet find(BlockPos origin, int amount, Connectivity connectivity, Type depth, Predicate<BlockPos> predicate) {
         LongSet set = new LongArraySet();
         Deque<BlockPos> ends = new ArrayDeque<>();
         ends.push(origin);
         BlockPos.Mutable mutable = origin.mutableCopy();
-        while(amount > 0) {
-            if(ends.isEmpty()) {
+        while (amount > 0) {
+            if (ends.isEmpty()) {
                 return set;
             }
             BlockPos pos;
-            switch(depth) {
+            switch (depth) {
                 case BREADTH:
                 default:
                     pos = ends.pollLast();
@@ -80,25 +47,25 @@ public final class BucketScanner {
                     pos = ends.pollFirst();
                     break;
             }
-            if(connectivity == Connectivity.TWENTY_SIX) {
-                for(int i = -1; i <= 1; i += 2) {
-                    for(int j = -1; j <= 1; j += 2) {
-                        for(int k = -1; k <= 1; k += 2) {
+            if (connectivity == Connectivity.TWENTY_SIX) {
+                for (int i = -1; i <= 1; i += 2) {
+                    for (int j = -1; j <= 1; j += 2) {
+                        for (int k = -1; k <= 1; k += 2) {
                             scan(mutable.set(pos.add(i, j, k)), set, ends, predicate);
                         }
                     }
                 }
             }
-            if(connectivity != Connectivity.SIX) {
-                for(int i = -1; i <= 1; i += 2) {
-                    for(int j = -1; j <= 1; j += 2) {
+            if (connectivity != Connectivity.SIX) {
+                for (int i = -1; i <= 1; i += 2) {
+                    for (int j = -1; j <= 1; j += 2) {
                         scan(mutable.set(pos.add(i, j, 0)), set, ends, predicate);
                         scan(mutable.set(pos.add(0, i, j)), set, ends, predicate);
                         scan(mutable.set(pos.add(j, 0, i)), set, ends, predicate);
                     }
                 }
             }
-            for(Direction direction : Direction.values()) {
+            for (Direction direction : Direction.values()) {
                 scan(mutable.set(pos.offset(direction)), set, ends, predicate);
             }
             set.add(pos.asLong());
@@ -107,8 +74,41 @@ public final class BucketScanner {
         return set;
     }
 
+    /**
+     * Finds any connected blocks and puts them in a {@link LongSet}.
+     *
+     * @param origin the position of the first block
+     * @param amount the amount of maximum blocks to find
+     * @param block the block type that can be accepted in the set
+     */
+    public static LongSet find(BlockPos origin, int amount, Connectivity connectivity, Type depth, Block block, ServerWorld world) {
+        return find(origin, amount, connectivity, depth, pos -> world.getBlockState(pos).isOf(block));
+    }
+
+    /**
+     * Finds any connected blocks and puts them in a {@link LongSet}.
+     *
+     * @param origin the position of the first block
+     * @param amount the amount of maximum blocks to find
+     * @param tag the tag for the blocks that can be accepted in the set
+     */
+    public static LongSet find(BlockPos origin, int amount, Connectivity connectivity, Type depth, Tag<Block> tag, ServerWorld world) {
+        return find(origin, amount, connectivity, depth, pos -> world.getBlockState(pos).isIn(tag));
+    }
+
+    /**
+     * Finds any connected blocks and puts them in a {@link LongSet}.
+     *
+     * @param origin the position of the first block
+     * @param amount the amount of maximum blocks to find
+     * @param ruleTest the rule test for the blocks that can be accepted in the set
+     */
+    public static LongSet find(BlockPos origin, int amount, Connectivity connectivity, Type depth, RuleTest ruleTest, ServerWorld world, Random random) {
+        return find(origin, amount, connectivity, depth, pos -> ruleTest.test(world.getBlockState(pos), random));
+    }
+
     private static void scan(BlockPos.Mutable mutable, LongSet set, Deque<BlockPos> ends, Predicate<BlockPos> predicate) {
-        if(predicate.test(mutable) && !set.contains(mutable.asLong()) && !ends.contains(mutable)) {
+        if (predicate.test(mutable) && !set.contains(mutable.asLong()) && !ends.contains(mutable)) {
             ends.push(mutable.toImmutable());
         }
     }

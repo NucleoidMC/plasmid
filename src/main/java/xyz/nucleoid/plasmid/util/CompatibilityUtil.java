87b72dfc6bcfe6c1f960f431afa278d6344faca9
Patbox <39821509+Patbox@users.noreply.github.com>
2021-05-20 21:27:55 +0200

    Move getting height from disguiselib to it's own method

--- src/main/java/xyz/nucleoid/plasmid/util/CompatibilityUtil.java
+++ src/main/java/xyz/nucleoid/plasmid/util/CompatibilityUtil.java
@@ -8,10 +8,19 @@ public class CompatibilityUtil {
     public static final boolean DISGUISELIB_COMPATIBILITY = FabricLoader.getInstance().isModLoaded("disguiselib");
 
     public static double getClientEntityHeight(Entity entity) {
-        if (DISGUISELIB_COMPATIBILITY && entity instanceof EntityDisguise && ((EntityDisguise) entity).isDisguised()) {
+        if (DISGUISELIB_COMPATIBILITY) {
+            return getDisguisedHeight(entity)
+        }
+
+        return entity.getHeight();
+    }
+
+    private static double getDisguisedHeight(Entity entity) {
+        if (entity instanceof EntityDisguise && ((EntityDisguise) entity).isDisguised()) {
             return ((EntityDisguise) entity).getDisguiseEntity().getHeight();
         }
 
         return entity.getHeight();
+
     }
 }

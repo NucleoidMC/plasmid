d536991f0b5d7165ea2ebabe9e5e4eb54efab5ff
Gegy <gegy1000@gmail.com>
2021-07-11 11:05:13 +0200

    set default game rules on GameSpace worlds

--- src/main/java/xyz/nucleoid/plasmid/game/world/GameSpaceWorlds.java
+++ src/main/java/xyz/nucleoid/plasmid/game/world/GameSpaceWorlds.java
@@ -5,13 +5,18 @@ import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import xyz.nucleoid.fantasy.Fantasy;
 import xyz.nucleoid.fantasy.RuntimeWorldConfig;
 import xyz.nucleoid.fantasy.RuntimeWorldHandle;
+import xyz.nucleoid.fantasy.util.GameRuleStore;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
 
 public final class GameSpaceWorlds implements Iterable<ServerWorld> {
     private final MinecraftServer server;
@@ -23,8 +28,11 @@ public final class GameSpaceWorlds implements Iterable<ServerWorld> {
     }
 
     public RuntimeWorldHandle add(RuntimeWorldConfig worldConfig) {
+        applyDefaultsTo(worldConfig);
+
         var worldHandle = Fantasy.get(this.server).openTemporaryWorld(worldConfig);
         this.worlds.put(worldHandle.asWorld().getRegistryKey(), worldHandle);
+
         return worldHandle;
     }
 
@@ -51,5 +59,21 @@ public final class GameSpaceWorlds implements Iterable<ServerWorld> {
     @Override
     public Iterator<ServerWorld> iterator() {
         return Iterators.transform(this.worlds.values().iterator(), RuntimeWorldHandle::asWorld);
+    }
+
+    private static void applyDefaultsTo(RuntimeWorldConfig worldConfig) {
+        var rules = worldConfig.getGameRules();
+
+        setDefaultRule(rules, GameRules.ANNOUNCE_ADVANCEMENTS, false);
+        setDefaultRule(rules, GameRules.DO_DAYLIGHT_CYCLE, false);
+        setDefaultRule(rules, GameRules.DO_WEATHER_CYCLE, false);
+        setDefaultRule(rules, GameRules.KEEP_INVENTORY, false);
+        setDefaultRule(rules, GameRules.DO_MOB_SPAWNING, false);
+    }
+
+    private static void setDefaultRule(GameRuleStore rules, GameRules.Key<GameRules.BooleanRule> key, boolean value) {
+        if (!rules.contains(key)) {
+            rules.set(key, value);
+        }
     }
 }

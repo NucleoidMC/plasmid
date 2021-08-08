94d994ee5434f9b3798aa5980d9020e30625ee90
Tom_The_Geek <tomthegeek.8559@gmail.com>
2021-08-08 18:16:25 +0100

    GameStatisticBundles should also have translation keys

--- src/main/java/xyz/nucleoid/plasmid/game/stats/GameStatisticBundle.java
+++ src/main/java/xyz/nucleoid/plasmid/game/stats/GameStatisticBundle.java
@@ -11,6 +11,8 @@ import java.util.UUID;
 
 /**
  * Wrapper containing a map players' {@link StatisticMap}s
+ * Similarly to {@link StatisticKey}s, {@link GameStatisticBundle}s should provide a translation
+ * key for their namespace in the form <code>statistic.bundle.[namespace]</code> for future-proofing
  */
 public class GameStatisticBundle {
     private final Object2ObjectMap<UUID, StatisticMap> players = new Object2ObjectOpenHashMap<>();
@@ -60,6 +62,10 @@ public class GameStatisticBundle {
                 throw new IllegalArgumentException("Bundle namespaces can only contain [a-zA-Z0-9_]");
             }
         }
+    }
+
+    public static String getTranslationKey(String namespace) {
+        return "statistic.bundle." + namespace;
     }
 
     private static boolean validateNamespaceChar(char c) {

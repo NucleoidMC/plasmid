b3924c4ddb48f4eb204520e7206dd44db0bb875c
Patbox <39821509+Patbox@users.noreply.github.com>
2021-10-02 12:13:38 +0200

    Update MenuPortalBackend/Config to match changes in GameConfig. Add page support to GameJoinUi

--- src/main/java/xyz/nucleoid/plasmid/command/ui/GameJoinUi.java
+++ src/main/java/xyz/nucleoid/plasmid/command/ui/GameJoinUi.java
@@ -1,28 +1,43 @@
 package xyz.nucleoid.plasmid.command.ui;
 
 import eu.pb4.sgui.api.elements.GuiElementBuilder;
+import eu.pb4.sgui.api.elements.GuiElementInterface;
 import eu.pb4.sgui.api.gui.SimpleGui;
+import net.minecraft.item.Items;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.math.MathHelper;
 import xyz.nucleoid.plasmid.game.GameSpace;
 import xyz.nucleoid.plasmid.game.manager.GameSpaceManager;
 import xyz.nucleoid.plasmid.game.manager.ManagedGameSpace;
 import xyz.nucleoid.plasmid.game.player.GamePlayerJoiner;
+import xyz.nucleoid.plasmid.util.Guis;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 
-// Todo: Maybe add paging support?
 public class GameJoinUi extends SimpleGui {
+    private static final GuiElementInterface EMPTY = new GuiElementBuilder(Items.GRAY_STAINED_GLASS_PANE).setName(LiteralText.EMPTY.copy()).build();
+
+    private static final int NAVBAR_POS = 81;
     private int tick;
+    private int page = 0;
+    private int pageSize;
 
     public GameJoinUi(ServerPlayerEntity player) {
         super(ScreenHandlerType.GENERIC_9X6, player, true);
         this.setTitle(new TranslatableText("text.plasmid.ui.game_join.title"));
-        this.updateGames();
+        this.updateUi();
+    }
+
+    private static void tryJoinGame(ServerPlayerEntity player, GameSpace gameSpace) {
+        player.server.submit(() -> {
+            var results = GamePlayerJoiner.tryJoin(player, gameSpace);
+            results.sendErrorsTo(player);
+        });
     }
 
     @Override
@@ -30,27 +45,77 @@ public class GameJoinUi extends SimpleGui {
         super.onTick();
         this.tick++;
         if (this.tick % 20 == 0) {
-            this.updateGames();
+            this.updateUi();
         }
     }
 
-    private void updateGames() {
+    private void updateUi() {
         int i = 0;
+        int gameI = 0;
 
         var games = new ArrayList<>(GameSpaceManager.get().getOpenGameSpaces());
         games.sort(Comparator.comparingInt(space -> space.getPlayers().size()));
 
-        for (ManagedGameSpace gameSpace : games) {
-            if (this.getFirstEmptySlot() != -1) {
-                this.setSlot(i++, this.createIconFor(gameSpace));
+        if (games.size() != 0) {
+            for (int x = 0; x < 183; x++) {
+                games.add(games.get(0));
             }
         }
 
-        for (; i < this.getSize(); i++) {
+        int limit = this.size;
+        this.pageSize = 0;
+
+        if (games.size() > this.size) {
+            limit = NAVBAR_POS;
+            this.pageSize = games.size() / NAVBAR_POS;
+        }
+
+        this.page = MathHelper.clamp(this.page, 0, this.pageSize);
+
+        for (ManagedGameSpace gameSpace : games) {
+            if (gameI >= this.page * NAVBAR_POS) {
+                if (i < limit) {
+                    this.setSlot(i++, this.createIconFor(gameSpace));
+                }
+            }
+            gameI++;
+        }
+
+        for (; i < limit; i++) {
             this.clearSlot(i);
+        }
+
+        if (this.pageSize != 0) {
+            boolean hasPrevious = this.page != 0;
+            boolean hasNext = this.page < this.pageSize;
+
+            this.setSlot(NAVBAR_POS, EMPTY);
+            this.setSlot(NAVBAR_POS + 1, EMPTY);
+
+            this.setSlot(NAVBAR_POS + 2, new GuiElementBuilder(hasPrevious ? Items.LIME_STAINED_GLASS_PANE : Items.BLACK_STAINED_GLASS_PANE)
+                    .setName(new TranslatableText("spectatorMenu.previous_page").formatted(hasPrevious ? Formatting.GOLD : Formatting.DARK_GRAY))
+                    .setCallback((x, y, z) -> this.changePage(-1))
+            );
+            int pageValue = this.page + 1;
+
+            this.setSlot(NAVBAR_POS + 3, Guis.getNumericBanner(pageValue / 100));
+            this.setSlot(NAVBAR_POS + 4, Guis.getNumericBanner(pageValue / 10));
+            this.setSlot(NAVBAR_POS + 5, Guis.getNumericBanner(pageValue));
+
+            this.setSlot(NAVBAR_POS + 6, new GuiElementBuilder(hasNext ? Items.LIME_STAINED_GLASS_PANE : Items.BLACK_STAINED_GLASS_PANE)
+                    .setName(new TranslatableText("spectatorMenu.next_page").formatted(hasNext ? Formatting.GOLD : Formatting.DARK_GRAY))
+                    .setCallback((x, y, z) -> this.changePage(1))
+            );
+
+            this.setSlot(NAVBAR_POS + 7, EMPTY);
+            this.setSlot(NAVBAR_POS + 8, EMPTY);
         }
     }
 
+    private void changePage(int change) {
+        this.page = MathHelper.clamp(this.page + change, 0, this.pageSize);
+        this.updateUi();
+    }
 
     private GuiElementBuilder createIconFor(GameSpace gameSpace) {
         var sourceConfig = gameSpace.getMetadata().sourceConfig();
@@ -76,12 +141,5 @@ public class GameJoinUi extends SimpleGui {
         element.setCallback((a, b, c, d) -> tryJoinGame(this.getPlayer(), gameSpace));
 
         return element;
-    }
-
-    private static void tryJoinGame(ServerPlayerEntity player, GameSpace gameSpace) {
-        player.server.submit(() -> {
-            var results = GamePlayerJoiner.tryJoin(player, gameSpace);
-            results.sendErrorsTo(player);
-        });
     }
 }
--- src/main/java/xyz/nucleoid/plasmid/game/portal/menu/MenuPortalConfig.java
+++ src/main/java/xyz/nucleoid/plasmid/game/portal/menu/MenuPortalConfig.java
@@ -1,44 +1,48 @@
 package xyz.nucleoid.plasmid.game.portal.menu;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import xyz.nucleoid.codecs.MoreCodecs;
 import xyz.nucleoid.plasmid.game.config.CustomValuesConfig;
 import xyz.nucleoid.plasmid.game.portal.GamePortalBackend;
 import xyz.nucleoid.plasmid.game.portal.GamePortalConfig;
+import xyz.nucleoid.plasmid.util.PlasmidCodecs;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 public record MenuPortalConfig(
-        String translation,
+        Text name,
+        List<Text> description,
+        ItemStack icon,
         List<Entry> games,
         CustomValuesConfig custom
 ) implements GamePortalConfig {
+
     public static final Codec<MenuPortalConfig> CODEC = RecordCodecBuilder.create(instance -> {
         return instance.group(
-                Codec.STRING.optionalFieldOf("translation").forGetter(config -> Optional.ofNullable(config.translation)),
+                PlasmidCodecs.TEXT.optionalFieldOf("name", LiteralText.EMPTY).forGetter(MenuPortalConfig::name),
+                MoreCodecs.listOrUnit(PlasmidCodecs.TEXT).optionalFieldOf("description", Collections.emptyList()).forGetter(MenuPortalConfig::description),
+                MoreCodecs.ITEM_STACK.optionalFieldOf("icon", new ItemStack(Items.GRASS_BLOCK)).forGetter(MenuPortalConfig::icon),
                 Entry.CODEC.listOf().fieldOf("games").forGetter(config -> config.games),
                 CustomValuesConfig.CODEC.optionalFieldOf("custom", CustomValuesConfig.empty()).forGetter(config -> config.custom)
         ).apply(instance, MenuPortalConfig::new);
     });
 
-    private MenuPortalConfig(Optional<String> translation, List<Entry> games, CustomValuesConfig custom) {
-        this(translation.orElse(null), games, custom);
-    }
-
     @Override
     public GamePortalBackend createBackend(MinecraftServer server, Identifier id) {
         Text name;
-        if (this.translation != null) {
-            name = new TranslatableText(this.translation);
+        if (this.name != null) {
+            name = this.name;
         } else {
             name = new LiteralText(id.toString());
         }
@@ -51,12 +55,21 @@ public record MenuPortalConfig(
         return CODEC;
     }
 
-    public record Entry(Identifier game, ItemStack icon) {
-        public static final Codec<Entry> CODEC = RecordCodecBuilder.create(instance -> {
+    public record Entry(Identifier game,
+                        Optional<Text> name,
+                        Optional<List<Text>> description,
+                        Optional<ItemStack> icon) {
+
+        static final Codec<Entry> CODEC_OBJECT = RecordCodecBuilder.create(instance -> {
             return instance.group(
                     Identifier.CODEC.fieldOf("game").forGetter(entry -> entry.game),
-                    MoreCodecs.ITEM_STACK.optionalFieldOf("icon", new ItemStack(Blocks.WHITE_WOOL)).forGetter(entry -> entry.icon)
+                    PlasmidCodecs.TEXT.optionalFieldOf("name").forGetter(Entry::name),
+                    MoreCodecs.listOrUnit(PlasmidCodecs.TEXT).optionalFieldOf("description").forGetter(Entry::description),
+                    MoreCodecs.ITEM_STACK.optionalFieldOf("icon").forGetter(Entry::icon)
             ).apply(instance, Entry::new);
         });
+
+        public static final Codec<Entry> CODEC = Codec.either(Identifier.CODEC, CODEC_OBJECT)
+                .xmap(either -> either.map((identifier) -> new Entry(identifier, Optional.empty(), Optional.empty(), Optional.empty()), Function.identity()), Either::right);
     }
 }

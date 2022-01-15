4f65bc5408d7a3345a294ca6ec8ffe18f3412583
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-01-15 01:40:25 -0500

    Add a codec for statistic bundle namespaces (#204)
    Fixes #203

--- src/main/java/xyz/nucleoid/plasmid/game/stats/GameStatisticBundle.java
+++ src/main/java/xyz/nucleoid/plasmid/game/stats/GameStatisticBundle.java
@@ -1,6 +1,8 @@
 package xyz.nucleoid.plasmid.game.stats;
 
 import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -8,6 +10,7 @@ import xyz.nucleoid.plasmid.util.PlayerRef;
 
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
 
 /**
  * Wrapper containing a map players' {@link StatisticMap}s
@@ -15,6 +18,19 @@ import java.util.UUID;
  * key for their namespace in the form <code>statistic.bundle.[namespace]</code>
  */
 public class GameStatisticBundle {
+    private static final String NAMESPACE_ERROR_MESSAGE = "Bundle namespaces can only contain [a-zA-Z0-9_]";
+    public static final Codec<String> NAMESPACE_CODEC = Codec.STRING.comapFlatMap(
+            string -> {
+                for (char c : string.toCharArray()) {
+                    if (!validateNamespaceChar(c)) {
+                        return DataResult.error(NAMESPACE_ERROR_MESSAGE);
+                    }
+                }
+                return DataResult.success(string);
+            },
+            Function.identity()
+    );
+
     private final Object2ObjectMap<UUID, StatisticMap> players = new Object2ObjectOpenHashMap<>();
     private final StatisticMap global = new StatisticMap();
 
@@ -60,7 +76,7 @@ public class GameStatisticBundle {
     public static void validateNamespace(String namespace) {
         for (char c : namespace.toCharArray()) {
             if (!validateNamespaceChar(c)) {
-                throw new IllegalArgumentException("Bundle namespaces can only contain [a-zA-Z0-9_]");
+                throw new IllegalArgumentException(NAMESPACE_ERROR_MESSAGE);
             }
         }
     }

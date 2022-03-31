831557d74d7e6519c7319e06c8f826cd4efc32e8
Restioson <restiosondev@gmail.com>
2022-04-01 00:37:18 +0200

    handle incoming events

--- plasmid-rs/src/lib.rs
+++ plasmid-rs/src/lib.rs
@@ -1,3 +1,4 @@
+use std::pin::Pin;
 use anyhow::Error;
 use futures::{Sink, SinkExt, Stream, StreamExt};
 use tokio::net::TcpStream;
@@ -5,11 +6,13 @@ use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 use glam::{IVec3, Vec3};
 use bytes::Bytes;
+use async_trait::async_trait;
+use xtra::{Actor, Address, Context, Handler, Message, spawn::TokioGlobalSpawnExt};
 
 const MAX_FRAME_LENGTH: usize = 4 * 1024 * 1024;
 const FRAME_HEADER_SIZE: usize = 4;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(transparent)]
 pub struct PlayerRef(Uuid);
 
@@ -18,8 +21,6 @@ pub type Identifier = String;
 #[derive(Serialize)]
 #[serde(tag = "type", content = "body")]
 pub enum Command {
-    #[serde(rename = "plasmid:get_participants")]
-    GetParticipants,
     #[serde(rename = "plasmid:teleport")]
     Teleport {
         player: PlayerRef,
@@ -38,7 +39,54 @@ pub enum Command {
     }
 }
 
-fn split_framed(stream: TcpStream) -> (impl Sink<Command, Error = Error> + Send, impl Stream<Item = String>) {
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type", content = "body")]
+pub enum Event {
+    #[serde(rename = "plasmid:participants")]
+    SetParticipants {
+        players: Vec<PlayerRef>,
+    },
+}
+
+impl Message for Event {
+    type Result = ();
+}
+
+pub struct Plasmid {
+    tx: Pin<Box<dyn Sink<Command, Error = Error> + Send + Sync + 'static>>,
+    participants: Vec<PlayerRef>,
+}
+
+#[async_trait]
+impl Actor for Plasmid {
+    type Stop = ();
+    async fn stopped(self) {}
+}
+
+#[async_trait]
+impl Handler<Event> for Plasmid {
+    async fn handle(&mut self, message: Event, _ctx: &mut Context<Self>) {
+        dbg!(message);
+    }
+}
+
+impl Plasmid {
+    pub async fn connect() -> Address<Self> {
+        let tcp = TcpStream::connect("localhost:12345").await.unwrap();
+        let (tx, rx) = split_framed(tcp);
+
+        let plasmid = Plasmid {
+            tx: Box::pin(tx),
+            participants: vec![],
+        };
+
+        let addr = plasmid.create(None).spawn_global();
+        tokio::spawn(addr.clone().attach_stream(rx));
+        addr
+    }
+}
+
+fn split_framed(stream: TcpStream) -> (impl Sink<Command, Error = Error> + Send, impl Stream<Item = Event>) {
     let (sink, stream) = tokio_util::codec::LengthDelimitedCodec::builder()
         .big_endian()
         .max_frame_length(MAX_FRAME_LENGTH)
@@ -56,7 +104,7 @@ fn split_framed(stream: TcpStream) -> (impl Sink<Command, Error = Error> + Send,
     });
 
     let stream = stream.map(|result| {
-        String::from_utf8(result.unwrap().to_vec()).unwrap()
+        serde_json::from_slice(&result.unwrap()).unwrap()
     });
 
     (sink, stream)

24e0caec83a4313eadaab5a35750cf60630333ed
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-08-05 22:43:16 -0400

    Ignore inactive players when detecting filled waiting lobbies
    Fixes #234

---
+++ src/main/java/xyz/nucleoid/plasmid/util/compatibility/AfkDisplayCompatibility.java
@@ -0,0 +1,23 @@
+package xyz.nucleoid.plasmid.util.compatibility;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import io.github.beabfc.afkdisplay.AfkPlayer;
+
+public final class AfkDisplayCompatibility {
+    private static final boolean ENABLED = FabricLoader.getInstance().isModLoaded("afkdisplay");
+
+    public static boolean isActive(ServerPlayerEntity player) {
+        if (ENABLED) {
+            return !isAfk(player);
+        } else {
+            return true;
+        }
+    }
+
+    private static boolean isAfk(ServerPlayerEntity player) {
+        var afkPlayer = (AfkPlayer) player;
+        return afkPlayer.isAfk();
+    }
+}

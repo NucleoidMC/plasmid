224c748b90896f4a3874e26135d78879eb0f0e1e
Gegy <gegy.dev@gmail.com>
2024-06-08 18:08:38 +0200

    Replace custom Codec implementation with regular dispatch

--- src/main/java/xyz/nucleoid/plasmid/game/config/GameConfig.java
+++ src/main/java/xyz/nucleoid/plasmid/game/config/GameConfig.java
@@ -1,7 +1,7 @@
 package xyz.nucleoid.plasmid.game.config;
 
-import com.mojang.datafixers.util.Pair;
-import com.mojang.serialization.*;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -22,8 +22,6 @@ import xyz.nucleoid.server.translations.api.language.ServerLanguageDefinition;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 public record GameConfig<C>(
         GameType<C> type,
@@ -34,7 +32,7 @@ public record GameConfig<C>(
         CustomValuesConfig custom,
         C config
 ) {
-    public static final Codec<GameConfig<?>> DIRECT_CODEC = new ConfigCodec().codec();
+    public static final Codec<GameConfig<?>> DIRECT_CODEC = GameType.REGISTRY.dispatch(GameConfig::type, GameConfig::createTypedCodec);
     public static final Codec<RegistryEntry<GameConfig<?>>> CODEC = RegistryElementCodec.of(GameConfigs.REGISTRY_KEY, DIRECT_CODEC);
 
     public GameOpenProcedure openProcedure(MinecraftServer server) {
@@ -105,65 +103,27 @@ public record GameConfig<C>(
         return language.serverTranslations().contains(translationKey);
     }
 
-    static final class ConfigCodec extends MapCodec<GameConfig<?>> {
-        @Override
-        public <T> Stream<T> keys(DynamicOps<T> ops) {
-            return Stream.concat(Stream.of(ops.createString("type")), Metadata.MAP_CODEC.keys(ops));
-        }
-
-        @Override
-        public <T> DataResult<GameConfig<?>> decode(DynamicOps<T> ops, MapLike<T> input) {
-            if (ops.compressMaps()) {
-                return DataResult.error(() -> "Does not support compressed ops");
-            }
-
-            var typeResult = GameType.REGISTRY.decode(ops, input.get("type")).map(Pair::getFirst);
-
-            return typeResult.flatMap(type ->
-                    Metadata.MAP_CODEC.decode(ops, input).flatMap(metadata ->
-                            this.decodeConfig(ops, input, type.configCodec()).map(config ->
-                                    this.createConfigUnchecked(type, metadata, config)
-                            )
-                    )
-            );
-        }
-
-        private <T> DataResult<?> decodeConfig(DynamicOps<T> ops, MapLike<T> input, MapCodec<?> configCodec) {
-            return configCodec.decode(ops, input).map(Function.identity());
-        }
-
-        @SuppressWarnings("unchecked")
-        private <C> GameConfig<C> createConfigUnchecked(GameType<C> type, Metadata metadata, Object config) {
-            return new GameConfig<>(
-                    type,
-                    metadata.name.orElse(null), metadata.shortName.orElse(null), metadata.description.orElse(null),
-                    metadata.icon, metadata.custom,
-                    (C) config
-            );
-        }
-
-        @Override
-        public <T> RecordBuilder<T> encode(GameConfig<?> game, DynamicOps<T> ops, RecordBuilder<T> prefix) {
-            return this.encodeUnchecked(game, ops, prefix);
-        }
-
-        private <T, C> RecordBuilder<T> encodeUnchecked(GameConfig<C> game, DynamicOps<T> ops, RecordBuilder<T> prefix) {
-            prefix = game.type.configCodec().encode(game.config, ops, prefix);
-            prefix.add("type", GameType.REGISTRY.encodeStart(ops, game.type));
-
-            var metadata = new Metadata(
-                    Optional.ofNullable(game.name), Optional.ofNullable(game.shortName), Optional.ofNullable(game.description),
-                    game.icon, game.custom
-            );
-            prefix = Metadata.MAP_CODEC.encode(metadata, ops, prefix);
-
-            return prefix;
-        }
+    private static <C> MapCodec<GameConfig<C>> createTypedCodec(GameType<C> type) {
+        return RecordCodecBuilder.mapCodec(i -> i.group(
+                type.configCodec().forGetter(GameConfig::config),
+                Metadata.MAP_CODEC.forGetter(Metadata::from)
+        ).apply(i, (config, metadata) -> new GameConfig<>(
+                type,
+                metadata.name.orElse(null),
+                metadata.shortName.orElse(null),
+                metadata.description.orElse(null),
+                metadata.icon,
+                metadata.custom,
+                config
+        )));
     }
 
-    record Metadata(
-            Optional<Text> name, Optional<Text> shortName, Optional<List<Text>> description,
-            ItemStack icon, CustomValuesConfig custom
+    private record Metadata(
+            Optional<Text> name,
+            Optional<Text> shortName,
+            Optional<List<Text>> description,
+            ItemStack icon,
+            CustomValuesConfig custom
     ) {
         static final MapCodec<Metadata> MAP_CODEC = RecordCodecBuilder.mapCodec(i -> i.group(
                 PlasmidCodecs.TEXT.optionalFieldOf("name").forGetter(Metadata::name),
@@ -172,5 +132,15 @@ public record GameConfig<C>(
                 MoreCodecs.ITEM_STACK.optionalFieldOf("icon", new ItemStack(Items.GRASS_BLOCK)).forGetter(Metadata::icon),
                 CustomValuesConfig.CODEC.fieldOf("custom").orElseGet(CustomValuesConfig::empty).forGetter(Metadata::custom)
         ).apply(i, Metadata::new));
+
+        public static Metadata from(GameConfig<?> game) {
+            return new Metadata(
+                    Optional.ofNullable(game.name),
+                    Optional.ofNullable(game.shortName),
+                    Optional.ofNullable(game.description),
+                    game.icon,
+                    game.custom
+            );
+        }
     }
 }

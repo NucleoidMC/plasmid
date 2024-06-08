9b7b99c38eba76e183baab28082e6f48303a5b1c
Gegy <gegy.dev@gmail.com>
2024-06-08 17:34:39 +0200

    Remove deprecated StorageType

--- src/main/java/xyz/nucleoid/plasmid/game/stats/StatisticKeys.java
+++ src/main/java/xyz/nucleoid/plasmid/game/stats/StatisticKeys.java
@@ -11,21 +11,21 @@ import xyz.nucleoid.plasmid.Plasmid;
 public final class StatisticKeys {
     // Games
     // These are hidden as typically the value for a given game will be 0 or 1
-    public static final StatisticKey<Integer> GAMES_PLAYED = StatisticKey.hiddenIntKey(id("games_played"), StatisticKey.StorageType.TOTAL);
-    public static final StatisticKey<Integer> GAMES_WON = StatisticKey.hiddenIntKey(id("games_won"), StatisticKey.StorageType.TOTAL);
-    public static final StatisticKey<Integer> GAMES_LOST = StatisticKey.hiddenIntKey(id("games_lost"), StatisticKey.StorageType.TOTAL);
+    public static final StatisticKey<Integer> GAMES_PLAYED = StatisticKey.intKey(id("games_played")).hidden(true);
+    public static final StatisticKey<Integer> GAMES_WON = StatisticKey.intKey(id("games_won")).hidden(true);
+    public static final StatisticKey<Integer> GAMES_LOST = StatisticKey.intKey(id("games_lost")).hidden(true);
 
     // PvP / PvE;
-    public static final StatisticKey<Integer> KILLS = StatisticKey.intKey(id("total_kills"), StatisticKey.StorageType.TOTAL);
-    public static final StatisticKey<Integer> DEATHS = StatisticKey.intKey(id("deaths"), StatisticKey.StorageType.TOTAL);
-    public static final StatisticKey<Double> DAMAGE_TAKEN = StatisticKey.doubleKey(id("damage_taken"), StatisticKey.StorageType.TOTAL);
-    public static final StatisticKey<Double> DAMAGE_DEALT = StatisticKey.doubleKey(id("damage_dealt"), StatisticKey.StorageType.TOTAL);
+    public static final StatisticKey<Integer> KILLS = StatisticKey.intKey(id("total_kills"));
+    public static final StatisticKey<Integer> DEATHS = StatisticKey.intKey(id("deaths"));
+    public static final StatisticKey<Double> DAMAGE_TAKEN = StatisticKey.doubleKey(id("damage_taken"));
+    public static final StatisticKey<Double> DAMAGE_DEALT = StatisticKey.doubleKey(id("damage_dealt"));
 
     // Misc;
-    public static final StatisticKey<Integer> BLOCKS_BROKEN = StatisticKey.intKey(id("blocks_broken"), StatisticKey.StorageType.TOTAL);
-    public static final StatisticKey<Integer> POINTS = StatisticKey.intKey(id("points"), StatisticKey.StorageType.TOTAL);
-    public static final StatisticKey<Integer> QUICKEST_TIME = StatisticKey.intKey(id("quickest_time"), StatisticKey.StorageType.MIN);
-    public static final StatisticKey<Integer> LONGEST_TIME = StatisticKey.intKey(id("longest_time"), StatisticKey.StorageType.MAX);
+    public static final StatisticKey<Integer> BLOCKS_BROKEN = StatisticKey.intKey(id("blocks_broken"));
+    public static final StatisticKey<Integer> POINTS = StatisticKey.intKey(id("points"));
+    public static final StatisticKey<Integer> QUICKEST_TIME = StatisticKey.intKey(id("quickest_time"));
+    public static final StatisticKey<Integer> LONGEST_TIME = StatisticKey.intKey(id("longest_time"));
 
     private static Identifier id(String path) {
         return new Identifier(Plasmid.ID, path);

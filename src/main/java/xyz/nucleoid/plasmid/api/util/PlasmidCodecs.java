7233a2f4d959932b5ea657c029e6bef2c393b6a1
Patbox <39821509+Patbox@users.noreply.github.com>
2024-11-15 20:55:07 +0100

    Update parser used in the codec

--- src/main/java/xyz/nucleoid/plasmid/api/util/PlasmidCodecs.java
+++ src/main/java/xyz/nucleoid/plasmid/api/util/PlasmidCodecs.java
@@ -2,7 +2,9 @@ package xyz.nucleoid.plasmid.api.util;
 
 import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
+import eu.pb4.placeholders.api.ParserContext;
 import eu.pb4.placeholders.api.TextParserUtils;
+import eu.pb4.placeholders.api.parsers.TagParser;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextCodecs;
 import net.minecraft.util.dynamic.Codecs;
@@ -11,7 +13,7 @@ import java.util.function.Function;
 
 public final class PlasmidCodecs {
     public static Codec<Text> TEXT = Codec.either(Codec.STRING, TextCodecs.CODEC)
-            .xmap(either -> either.map(TextParserUtils::formatTextSafe, Function.identity()), Either::right);
+            .xmap(either -> either.map((s) -> TagParser.QUICK_TEXT_WITH_STF.parseText(s, ParserContext.of()), Function.identity()), Either::right);
 
     private PlasmidCodecs() {}
 

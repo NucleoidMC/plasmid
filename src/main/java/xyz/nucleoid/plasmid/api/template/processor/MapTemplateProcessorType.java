866cd30ec042a527366233f081f03ba031808918
Hugman <contact.hugman@gmail.com>
2025-08-05 22:35:00 +0200

    Sort registry entries

--- src/main/java/xyz/nucleoid/plasmid/api/template/processor/MapTemplateProcessorType.java
+++ src/main/java/xyz/nucleoid/plasmid/api/template/processor/MapTemplateProcessorType.java
@@ -7,9 +7,9 @@ import xyz.nucleoid.plasmid.api.registry.PlasmidRegistries;
 import xyz.nucleoid.plasmid.impl.Plasmid;
 
 public record MapTemplateProcessorType<T extends MapTemplateProcessor>(MapCodec<T> codec) {
-    public static final MapTemplateProcessorType<TeamColorMapTemplateProcessor> TEAM_COLORS = of("team_colors", TeamColorMapTemplateProcessor.CODEC);
     public static final MapTemplateProcessorType<ReplaceBlocksTemplateProcessor> REPLACE_BLOCKS = of("replace_blocks", ReplaceBlocksTemplateProcessor.CODEC);
     public static final MapTemplateProcessorType<ReplaceBlockEntitiesTemplateProcessor> REPLACE_BLOCK_ENTITIES = of("replace_block_entities", ReplaceBlockEntitiesTemplateProcessor.CODEC);
+    public static final MapTemplateProcessorType<TeamColorMapTemplateProcessor> TEAM_COLORS = of("team_colors", TeamColorMapTemplateProcessor.CODEC);
 
     private static <T extends MapTemplateProcessor> MapTemplateProcessorType<T> of(String name, MapCodec<T> codec) {
         return of(Identifier.of(Plasmid.ID, name), codec);
